38c1e51d448372948933608e5ae9ddc0bf77de8b
zogeek <ziyadossart@gmail.com>
2025-01-06 10:13:31 +0100

    feat : linearRegression IA

--- second_year/python/data/scrapping/ia.py
+++ second_year/python/data/scrapping/ia.py
@@ -1,5 +1,21 @@
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 def read_data():
     return pd.read_csv("data.csv")
+
+def linear_regression(data):
+    lireg = LinearRegression()
+
+    learnData = data[['km']]
+    target = data[['price']]
+    print(learnData)
+    lireg.fit(learnData, target)
+    result = lireg.predict([[100000]])
+    print(f"Le prix du céhicule sera{result[0][0]}")
+def main():
+    data = read_data()
+    linear_regression(data)
+
+main()
